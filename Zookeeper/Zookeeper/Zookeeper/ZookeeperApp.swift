c16c0022817f767bac35586bf255c7a1ed2dbc8e
Cody <66153585+nonstickcode@users.noreply.github.com>
2022-04-07 12:58:46 -0700

    Add files via upload

---
+++ Zookeeper/Zookeeper/Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by Cody McRoy on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
